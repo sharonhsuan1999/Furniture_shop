855a829a4f87d29c6e0b057a47143ae619fec675
wvvch <wvvchgaming@gmail.com>
2022-01-02 18:42:21 +0800

    01020642

--- 家具網頁/web1/web1/ShoppingCart.aspx.cs
+++ 家具網頁/web1/web1/ShoppingCart.aspx.cs
@@ -11,39 +11,78 @@ namespace web1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Session["customer_id"] =1;
-            countTotal();
+            Session["customer_id"] =2;
+            //countTotal();
         }
         protected void CartView_SelectedIndexChanged(object sender, EventArgs e)
         {
         }
+       
+
         protected void CartView_RowDataBound(object sender, GridViewRowEventArgs e)
         {
+            countTotal();
         }
 
         private void countTotal()
         {
-            string getTotal = " ";
             int total = 0;
-            //char[] MyChar = { ',', '.'};
+            string warnMsg = "";
 
             for (int i = 0; i < CartView.Rows.Count; i++)
             {
-                
+
                 if (CartView.Rows[i].Cells[3].FindControl("itemNumLB") != null)
                 {
-                    getTotal = ((Label)CartView.Rows[i].Cells[2].FindControl("subtotalLB")).Text;
-                    //getTotal= getTotal.Remove(0, 3);
-                    //getTotal = getTotal.Replace(",","");
-                    //getTotal = getTotal.Replace(".", "");
-
-                    //total += ((Convert.ToInt32(((Label)CartView.Rows[i].Cells[3].FindControl("itemNumLB")).Text))*(Convert.ToInt32(((Label)CartView.Rows[i].Cells[2].FindControl("subtotalLB")).Text)));
-                    //total += (Convert.ToInt32(getTotal))* (Convert.ToInt32(((Label)CartView.Rows[i].Cells[3].FindControl("itemNumLB")).Text));
-                }    
+                    total += ((Convert.ToInt32(((Label)CartView.Rows[i].Cells[3].FindControl("itemNumLB")).Text)) * (Convert.ToInt32(((Label)CartView.Rows[i].Cells[2].FindControl("subtotalLB")).Text)));
+                }
+                numEditCheck(ref warnMsg, i);
             }
 
-            CartTotalPrice.Text = "Total:" + getTotal;
+            CartTotalPrice.Text = "Total: NT " + total;
+            
+            if (0 == total)
+            {
+                PlaceOrder.Enabled = false;
+            }
         }
 
+        private void numEditCheck(ref string msg, int i)
+        {
+            if (CartView.Rows[i].Cells[3].FindControl("itemNumLB") != null)
+            {
+                using (Label tempNumLB = (Label)CartView.Rows[i].Cells[3].FindControl("itemNumLB"))
+                {
+                    if (tempNumLB.Text == "0")
+                    {
+                        msg = "(錯誤的數量)";
+                        tempNumLB.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        tempNumLB.ForeColor = System.Drawing.Color.Black;
+                    }
+                }
+            }
+        }
+
+        protected void CartView_RowDeleted(object sender, GridViewDeletedEventArgs e)
+        {
+            if (CartView.Rows.Count == 1)
+            {
+                CartTotalPrice.Text = "購物車為空，歡迎前往選購！";
+            }
+        }
+
+        protected void editNumLB_TextChanged(object sender, EventArgs e)
+        {
+            foreach (char ch in ((TextBox)sender).Text)
+            {
+                if (!Char.IsDigit(ch))
+                {
+                    ((TextBox)sender).Text = "0";
+                }
+            }
+        }
     }
 }
