fd1be2f217400829f46aa4189f9527af95a2ec69
wvvch <wvvchgaming@gmail.com>
2022-01-02 15:35:04 +0800

    0102

--- 家具網頁/web1/web1/ShoppingCart.aspx.cs
+++ 家具網頁/web1/web1/ShoppingCart.aspx.cs
@@ -13,6 +13,32 @@ namespace web1
         {
             Session["customer_id"] =1;
         }
+        protected void CartView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+            protected void CartView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            countTotal();
+        }
+
+        private void countTotal()
+        {
+            string total = " ";
+            //int total = 0;
+            
+            for(int i = 0; i < CartView.Rows.Count; i++)
+            {
+                if (CartView.Rows[i].Cells[2].FindControl("subtotalLB") != null) 
+                {
+                    //total = CartView.Rows[0].Cells[2].Text;
+                    total += ((Convert.ToInt32(((Label)CartView.Rows[i].Cells[3].FindControl("itemNumLB")).Text))*(Convert.ToInt32(((Label)CartView.Rows[i].Cells[2].FindControl("subtotalLB")).Text)));
+                }
+            }
+
+            CartTotalPrice.Text = "Total：" + total + "元";
+
+        }
 
     }
 }
